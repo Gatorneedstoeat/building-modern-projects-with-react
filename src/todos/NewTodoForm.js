83e6461a76494961bd38b4f6cdac5df068918e54
Sam Slosser <samuel.slosser@gmail.com>
2020-05-12 09:18:02 -0400

    finished 3-6

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { createTodo } from './actions';
 import './NewTodoForm.css';
 
-const NewTodoForm = () => {
+// this form will automaticly recieve the todo's from the state passed as a todos prop
+const NewTodoForm = ({ todos, onCreatePressed }) => {
     const [inputValue, setInputValue] = useState('');
 
     return (
@@ -12,9 +15,38 @@ const NewTodoForm = () => {
                 placeholder="Type your new todo here"
                 value={inputValue}
                 onChange={e => setInputValue(e.target.value)} />
-            <button className="new-todo-button">Create Todo</button>
+            <button 
+            onClick={()=>{
+                // check for a duplicate todo
+                //.some returns boolean value based on the value checked in the function
+                const isDuplicateText =
+                todos.some(todo => todo.text === inputValue)
+                if(!isDuplicateText){
+                //call dispatch function
+                onCreatePressed(inputValue);
+                // reset 
+                setInputValue('');
+                }
+                
+            }}
+            className="new-todo-button">Create Todo</button>
         </div>
     );
 };
+//returns an object { todos: state.todos }
+// state parameter is the entire redux state
+// mapStateToProps takes state object and returns another object containing pieces of the state the componet needs access to
+// this form will automaticly recieve the todo's from the state passed as a todos prop
+const mapStateToProps = state => ({
+    todos: state.todos,
+});
 
-export default NewTodoForm;
+// this function returns props and passed to this component
+// dispatch is a function that allows the components to trigger redux actions that the store will respond to
+// trigger a redux action when someone clicks the create todo button
+const mapDispatchToProps = dispatch => ({
+    onCreatePressed: text => dispatch(createTodo(text)),
+});
+// connect is a higher order function from react-redux, connect()() returns a connected version of the component
+// pass the above functions
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
