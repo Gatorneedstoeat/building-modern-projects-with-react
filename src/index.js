fa12f1e27f2dde43e9963ba991255446bdfdae4f
Sam Slosser <samuel.slosser@gmail.com>
2020-05-12 10:45:43 -0400

    finsihed 3-8 redux persistance

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 import App from './App.js';
 
+// define const to reuse configureStore() method
+const store = configureStore();
+
+const persistor = persistStore(store);
+
 ReactDOM.render(
-    <Provider store={configureStore()} >
+    <Provider store={store} >
+        {/* This delays the rendering of your app's UI until your persisted state has been retrieved and saved to redux. 
+        Loading attribute is to show view while page data is loading */}
+        <PersistGate
+            loading={<div>loading...</div>}
+            persistor={persistor} >
         <App />
-    </Provider>,
+        </PersistGate>
+    </Provider >,
     document.getElementById('root'),
 );
--- src/store.js
+++ src/store.js
@@ -1,12 +1,25 @@
 import { createStore, combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
 import { todos } from './todos/reducers';
 
 // reducer
 const reducers = {
     todos,
 };
-
+// object that tells redux-persit how to save and where to store app data
+// storage property defaults to local storage from import
+// stateReconciler: autoMergeLevel2 tell redux persist how to reconcile the initial and stored states of the app how deep should it go
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+}
 //put reducers into form that can pass into createStore function
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+// this wraps the rootReducer in a persitant container
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+export const configureStore = () => createStore(persistedReducer);
